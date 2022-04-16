fb0f7ca395c7dd20b223e367d52dd9ac3b5e4eac
junsu hwang <hjunsu0325@gmail.com>
2022-04-16 23:12:08 +0900

    BaseEntity 리팩토링

--- src/main/java/com/ecommerce/backend/BackendApplication.java
+++ src/main/java/com/ecommerce/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.ecommerce.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 	public static void main(String[] args) {
