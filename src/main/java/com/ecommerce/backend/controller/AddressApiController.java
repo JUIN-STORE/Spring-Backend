07b3aa75bdb15931f43445edbb8a47e15a6f2d7a
junsu hwang <junsu0325@naver.com>
2022-10-30 23:53:53 +0900

    ADDRESS-6 모든 주소 읽기 추가 및 테스트 코드 작성

--- src/main/java/com/ecommerce/backend/controller/AddressApiController.java
+++ src/main/java/com/ecommerce/backend/controller/AddressApiController.java
@@ -1,10 +1,12 @@
 package com.ecommerce.backend.controller;
 
 import com.ecommerce.backend.MyResponse;
+import com.ecommerce.backend.domain.entity.Account;
 import com.ecommerce.backend.domain.entity.Address;
 import com.ecommerce.backend.domain.request.AddressRequest;
 import com.ecommerce.backend.domain.response.AddressResponse;
 import com.ecommerce.backend.service.AddressService;
+import com.ecommerce.backend.service.JwtService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Api(tags = {"02. Address"})
 @Slf4j
@@ -21,27 +25,49 @@ import java.security.Principal;
 @RequiredArgsConstructor
 @RequestMapping("/api/addresses")
 public class AddressApiController {
+    private final JwtService jwtService;
+
     private final AddressService addressService;
 
     @ApiOperation(value = "주소 추가", notes="주소를 추가한다.")
-    @PostMapping("/new")
-    public MyResponse<Void> newAddress(@RequestBody AddressRequest.Register request, Principal principal) {
-        final String email = principal.getName();
-
-        log.info("POST /api/addresses/create -> email: {}, request: {}", email, request);
+    @PostMapping
+    public MyResponse<Void> newAddress(Principal principal,
+                                       @RequestBody AddressRequest.Register request) {
+        final Account account = jwtService.readByPrincipal(principal);
 
         try {
-            addressService.addAddress(request, principal);
+            addressService.addAddress(account, request);
             return new MyResponse<>(HttpStatus.OK, null);
         } catch (EntityNotFoundException e) {
-            log.warn("POST /api/addresses/create -> email: {}, request: {} EntityNotFoundException", email, request);
+            log.warn("POST /api/addresses -> request: {} EntityNotFoundException",  request);
+            return new MyResponse<>(HttpStatus.NOT_FOUND, null);
+        }
+    }
+
+    @ApiOperation(value = "모든 주소 읽기", notes="주소를 불러온다.")
+    @GetMapping("/all")
+    public MyResponse<List<AddressResponse.Read>> all(Principal principal) {
+        final Account account = jwtService.readByPrincipal(principal);
+
+        try {
+            final List<Address> addressList = addressService.readByAccountId(account.getId());
+            List<AddressResponse.Read> response = new ArrayList<>();
+
+            for (Address address : addressList) {
+                response.add(AddressResponse.Read.from(address));
+            }
+
+            return new MyResponse<>(HttpStatus.OK, response);
+        } catch (EntityNotFoundException e) {
+            log.warn("GET /api/addresses/all EntityNotFoundException");
             return new MyResponse<>(HttpStatus.NOT_FOUND, null);
         }
     }
 
     @ApiOperation(value = "단건 주소 읽기", notes="주소를 불러온다.")
     @GetMapping("/{addressId}")
-    public MyResponse<AddressResponse.Read> one(@PathVariable Long addressId) {
+    public MyResponse<AddressResponse.Read> one(Principal principal,
+                                                @PathVariable Long addressId) {
         log.info("GET /api/addresses/{addressId} addressId: {}", addressId);
 
         try {
@@ -56,13 +82,18 @@ public class AddressApiController {
     }
 
     @ApiOperation(value = "주소 수정", notes="주소를 수정한다.")
-    @PutMapping("/{addressId}")
-    public MyResponse<Void> update(@PathVariable Long addressId, @RequestBody AddressRequest.Update request){
-        log.info("PATCH /api/addresses/update/{addressId} -> request: {}", request);
-        try {
-            addressService.update(addressId, request);
+    @PutMapping
+    public MyResponse<Void> update(Principal principal,
+                                   @RequestBody AddressRequest.Update request) {
 
-            return new MyResponse<>(HttpStatus.OK, "GET SUCCESS", null);
+        log.info("PATCH /api/addresses/update/{addressId} -> request: {}", request);
+
+        jwtService.readByPrincipal(principal);
+
+        try {
+            addressService.update(request);
+
+            return new MyResponse<>(HttpStatus.OK, null);
         } catch (EntityNotFoundException e) {
             log.warn("PATCH /api/addresses/{addressId} -> request: {} EntityNotFoundException", request);
             return new MyResponse<>(HttpStatus.NOT_FOUND, "PATCH FAIL", null);
@@ -72,8 +103,10 @@ public class AddressApiController {
     // FIXME: 비정상적인 동작
     @ApiOperation(value = "주소 삭제", notes = "주소 정보를 삭제한다.")
     @DeleteMapping("/{addressId}")
-    public MyResponse<AddressResponse.Delete> delete(@PathVariable Long addressId){
+    public MyResponse<AddressResponse.Delete> delete(Principal principal,
+                                                     @PathVariable Long addressId){
         log.info("DELETE /api/addresses/{addressId} -> addressId: {}", addressId);
+        final Account account = jwtService.readByPrincipal(principal);
 
         try {
             final Address address = addressService.remove(addressId);
---
+++ src/test/java/com/ecommerce/backend/service/AddressServiceTest.java
@@ -0,0 +1,73 @@
+package com.ecommerce.backend.service;
+
+import com.ecommerce.backend.domain.entity.Address;
+import com.ecommerce.backend.repository.jpa.AddressRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class AddressServiceTest {
+    @InjectMocks
+    private AddressService sut;
+
+    @Mock
+    private AddressRepository addressRepository;
+
+    @Nested
+    @DisplayName("정상 케이스")
+    class Success {
+        private Address address;
+
+        @BeforeEach
+        void setUp() {
+            address = Address.builder()
+                    .id(1L)
+                    .city("서울시")
+                    .street("강남구")
+                    .zipCode(12345)
+                    .defaultAddress(false)
+                    .build();
+        }
+
+        @Test
+        @DisplayName("save")
+        void saveTest() {
+            // given
+            given(addressRepository.save(any(Address.class))).willReturn(address);
+
+            // when
+            final Address actual = sut.addAddress(address);
+
+            // then
+            assertThat(actual).isEqualTo(address);
+        }
+
+        @Test
+        @DisplayName("readById")
+        void readByIdTest() {
+            // given
+            given(addressRepository.findById(any(Long.class))).willReturn(Optional.of(address));
+
+            // when
+            final Address actual = sut.readById(1L);
+
+            // then
+            assertThat(actual).isEqualTo(address);
+        }
+
+        // FIXME: 테스트 코드 더 작성해야 됨.
+    }
+
+}
