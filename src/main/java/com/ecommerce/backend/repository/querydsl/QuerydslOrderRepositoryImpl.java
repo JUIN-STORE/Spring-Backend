5cd4d102b33a4f7139834980799c19f551725aea
jduckling1024 <jduck1024@naver.com>
2022-11-27 20:07:37 +0900

    ORDER-8 주문 조회 페이징 처리

--- src/main/java/com/ecommerce/backend/repository/querydsl/QuerydslOrderRepositoryImpl.java
+++ src/main/java/com/ecommerce/backend/repository/querydsl/QuerydslOrderRepositoryImpl.java
@@ -5,8 +5,12 @@ import com.ecommerce.backend.domain.request.OrderRequest;
 import com.ecommerce.backend.domain.response.OrderJoinResponse;
 import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Wildcard;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -31,36 +35,50 @@ public class QuerydslOrderRepositoryImpl implements QuerydslOrderRepository {
     }
 
     @Override
-    public Optional<List<OrderJoinResponse>> findOrderJoinOrderProductJoinProductByAccountId(Long accountId,
-                                                                                             OrderRequest.Read request) {
-        return Optional.ofNullable(
-                queryFactory
-                        .select(Projections.fields(OrderJoinResponse.class
-                                        , order.orderDate
-                                        , orderProduct.orderCount
-                                        , product.id.as("productId")
-                                        , product.price
-                                        , product.productName
-                                        , order.id.as("ordersId")
-                                        , orderProduct.product.id.as("orderProductId")
-                                        , order.delivery.id.as("deliveryId")
-                                        , order.orderStatus
-                                )
+    public Optional<Page<OrderJoinResponse>> findOrderJoinOrderProductJoinProductByAccountId(Long accountId,
+                                                                                             OrderRequest.Read request,
+                                                                                             Pageable pageable) {
+        // FIXME: 더 좋은 방법 있으면 변경하기
+        List<OrderJoinResponse> orderJoinResponseList = queryFactory
+                .select(Projections.fields(OrderJoinResponse.class
+                                , order.orderDate
+                                , orderProduct.orderCount
+                                , product.id.as("productId")
+                                , product.price
+                                , product.productName
+                                , order.id.as("ordersId")
+                                , orderProduct.product.id.as("orderProductId")
+                                , order.delivery.id.as("deliveryId")
+                                , order.orderStatus
                         )
-                        .from(order)
-                        .join(orderProduct)
-                        .on(order.id.eq(orderProduct.order.id))
-                        .join(product)
-                        .on(product.id.eq(orderProduct.product.id))
-                        .where(order.account.id.eq(accountId),
-                                orderDateBetween(
-                                        request.getStartDate().atStartOfDay(),
-                                        request.getEndDate().atStartOfDay()
-                                ),
-                                orderStatusEq(request.getOrderStatus()))
-                        .orderBy(order.id.desc())
-                        .fetch()
-        );
+                )
+                .from(order)
+                .join(orderProduct)
+                .on(order.id.eq(orderProduct.order.id))
+                .join(product)
+                .on(product.id.eq(orderProduct.product.id))
+                .where(order.account.id.eq(accountId),
+                        orderDateBetween(
+                                request.getStartDate().atStartOfDay(),
+                                request.getEndDate().atStartOfDay()
+                        ),
+                        orderStatusEq(request.getOrderStatus()))
+                .orderBy(order.id.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return Optional.of(new PageImpl<>(orderJoinResponseList, pageable, countOrderProduct(accountId)));
+    }
+
+    private Long countOrderProduct(Long accountId) {
+        List<Long> fetch = queryFactory.select(Wildcard.count)
+                .from(order)
+                .join(orderProduct)
+                .on(order.id.eq(orderProduct.order.id))
+                .where(order.account.id.eq(accountId))
+                .fetch();
+        return fetch.get(0);
     }
 
     private BooleanExpression orderDateBetween(LocalDateTime startDate, LocalDateTime endDate) {
