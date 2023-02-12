f0d5cfe9cf6b2fad59c88372f23a428e4bf8dc9e
HwangJunSu <hjunsu0325@gmail.com>
2023-02-12 17:31:38 +0900

    TC-22 DeliveryApiControllerTest 코드 작성 및 RestDocs 적용

--- src/test/java/store/juin/api/domain/EntityUtil.java
+++ src/test/java/store/juin/api/domain/EntityUtil.java
@@ -2,6 +2,9 @@ package store.juin.api.domain;
 
 import store.juin.api.domain.entity.Account;
 import store.juin.api.domain.entity.Address;
+import store.juin.api.domain.entity.Delivery;
+import store.juin.api.domain.entity.DeliveryReceiver;
+import store.juin.api.domain.enums.DeliveryStatus;
 
 public class EntityUtil {
     public static Account makeAccount() {
@@ -15,10 +18,27 @@ public class EntityUtil {
     public static Address makeAddress(boolean defaultAddress) {
         return Address.builder()
                 .id(3L)
-                .city("서울시")
-                .street("강남구")
+                .city("서울시 강남구 테헤란로 123")
+                .street("어디 건물 8층")
                 .zipCode(12345)
                 .defaultAddress(defaultAddress)
+                .build();
+    }
+
+    public static Delivery makeDelivery(Long id) {
+        return Delivery.builder()
+                .id(id)
+                .deliveryReceiver(makeDeliveryReceiver())
+                .deliveryAddress(makeAddress(true))
+                .deliveryStatus(DeliveryStatus.READY)
+                .build();
+    }
+
+    public static DeliveryReceiver makeDeliveryReceiver() {
+        return DeliveryReceiver.builder()
+                .receiverName("황준수")
+                .receiverEmail("js@gmail.com")
+                .receiverPhoneNumber("010-1234-5678")
                 .build();
     }
 }
