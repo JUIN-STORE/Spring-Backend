e9e58e280aaa18cc425e3b5cf5ef95ed202cdfe4
HwangJunSu <junsu0325@naver.com>
2023-03-12 23:33:25 +0900

    CART-3 /api/carts/count 추가

--- src/main/java/store/juin/api/repository/querydsl/QuerydslCartRepository.java
+++ src/main/java/store/juin/api/repository/querydsl/QuerydslCartRepository.java
@@ -2,7 +2,11 @@ package store.juin.api.repository.querydsl;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface QuerydslCartRepository {
     long deleteByAccountId(Long accountId);
+
+    Optional<Long> countItemsByAccountId(Long accountId);
 }
--- src/main/java/store/juin/api/repository/querydsl/impl/QuerydslCartRepositoryImpl.java
+++ src/main/java/store/juin/api/repository/querydsl/impl/QuerydslCartRepositoryImpl.java
@@ -1,11 +1,14 @@
 package store.juin.api.repository.querydsl.impl;
 
-import store.juin.api.repository.querydsl.QuerydslCartRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
+import store.juin.api.repository.querydsl.QuerydslCartRepository;
+
+import java.util.Optional;
 
 import static store.juin.api.domain.entity.QCart.cart;
+import static store.juin.api.domain.entity.QCartItem.cartItem;
 
 
 @RequiredArgsConstructor
@@ -19,5 +22,15 @@ public class QuerydslCartRepositoryImpl implements QuerydslCartRepository {
                 .delete(cart)
                 .where(cart.account.id.eq(accountId))
                 .execute();
+    }
+
+    @Override
+    public Optional<Long> countItemsByAccountId(Long accountId) {
+        return Optional.ofNullable(queryFactory
+                .select(cart.count())
+                .from(cart)
+                .join(cartItem).on(cartItem.cart.id.eq(cart.id))
+                .where(cart.account.id.eq(accountId))
+                .fetchOne());
     }
 }
