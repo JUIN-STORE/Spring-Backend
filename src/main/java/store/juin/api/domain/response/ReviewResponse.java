444e9d5e4220355346b3f2ed6777ec234f67e25f
YoonJin <jduck1024@naver.com>
2023-07-19 03:14:21 +0900

    REVIEW-2 response에 필요한 필드 추가

--- src/main/java/store/juin/api/domain/response/ReviewResponse.java
+++ src/main/java/store/juin/api/domain/response/ReviewResponse.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.Accessors;
 import store.juin.api.domain.entity.Review;
 
+import java.time.LocalDateTime;
+
 @NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
 public class ReviewResponse {
     @Data
@@ -12,18 +14,24 @@ public class ReviewResponse {
     public static class Read {
         private Long reviewId;
 
+        private Long accountId;
+
         private String accountName;
 
         private Integer rate;
 
         private String content;
 
+        private LocalDateTime createdAt;
+
         public static Read from(Review review) {
             return new Read()
                     .setReviewId(review.getReviewId())
+                    .setAccountId(review.getAccount().getId())
                     .setAccountName(review.getAccount().getName())
                     .setRate(review.getRate())
-                    .setContent(review.getContent());
+                    .setContent(review.getContent())
+                    .setCreatedAt(review.getCreatedAt());
         }
     }
 }
